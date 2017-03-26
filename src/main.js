733879d9e52657471c8107d05b3455a480653d43
Caitlin Rainone <caitlin.rainone@gmail.com>
2017-03-26 13:19:01 -0700

    Fixed eslint config for node.
    Signed-off-by: Caitlin Rainone <caitlin.rainone@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var fs = require('fs');
+var path = require('path')
+var express = require('express')
+var app = express()
+var fs = require('fs')
 
-var dir = path.join(__dirname, '..', 'public');
+var dir = path.join(__dirname, '..', 'public')
 
 var mime = {
   html: 'text/html',
@@ -14,28 +14,28 @@ var mime = {
   png: 'image/png',
   svg: 'image/svg+xml',
   js: 'application/javascript'
-};
-
-function getAPIResponse(request, response) {
-  console.log('accessed api');
 }
 
-function getFileResponse(request, response) {
-  console.log('getting file response of ' + request.url);
-  var file = path.join(dir, request.path.replace(/\/$/, '/index.html'));
+function getAPIResponse (request, response) {
+  console.log('accessed api')
+}
+
+function getFileResponse (request, response) {
+  console.log('getting file response of ' + request.url)
+  var file = path.join(dir, request.path.replace(/\/$/, '/index.html'))
   if (file.indexOf(dir + path.sep) !== 0) {
-    return response.status(403).end('Forbidden');
+    return response.status(403).end('Forbidden')
   }
-  
-  var type = mime[path.extname(file).slice(1)] || 'text/plain';
-  var s = fs.createReadStream(file);
+
+  var type = mime[path.extname(file).slice(1)] || 'text/plain'
+  var s = fs.createReadStream(file)
   s.on('open', function () {
-    response.set('Content-Type', type);
-      s.pipe(response);
-  });
+    response.set('Content-Type', type)
+    s.pipe(response)
+  })
   s.on('error', function (err) {
-    response.set('Content-Type', 'text/plain');
-    response.status(404).end('Not found');
+    response.set('Content-Type', 'text/plain')
+    response.status(404).end('Not found')
   });
 }
 
